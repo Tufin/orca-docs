8a219f755848862f198b09e9052f2d5e14f38ac4
Neil Leigh <neil.leigh@tufin.com>
2019-02-19 08:38:06 +0200

    Duplicated \Content from local working directory
    This is a new local repo based on https://github.com/Tufin/orca-docs. I just copied the latest version of C:\TufinDocs\Orca\Content to here and committed

---
+++ Content/Resources/Custom/custom.js
@@ -0,0 +1,9 @@
+﻿/* Version: 1.0 */
+$(function() { //jQuery ready function
+	// Set theme
+	var theme = $('#var-theme').text();
+	$('body').addClass('theme-' + theme);
+	
+	// Remove header
+	//$('.homepage nav.title-bar').remove();
+});
