f21651ccc96ad61e82f91e8ff8e9c55c73168e79
Neil Leigh <neil.leigh@tufin.com>
2019-02-19 09:03:11 +0200

    Remaining folders and flare project file
    Previously committed only content folder. Now remaining folders and flare project file

---
+++ Project/Advanced/HeaderFile.h
@@ -0,0 +1,13 @@
+
+
+
+
+#define Deploy 1000
+#define ServiceGraph 1002
+#define Pods 1003
+#define Services 1004
+#define Nodes 1005
+#define Policy 1006
+#define Settings 1007
+#define UnderstandingOrca 1008
+#define Vulnerabilities 1001
