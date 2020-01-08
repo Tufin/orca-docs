dcad2a274bfde94d384d1b8a4e42e8b59ce08ff0
Neil Leigh <neil.leigh@tufin.com>
2020-01-08 12:40:46 +0200

    fix / neil / next stage of securecloud merge

---
+++ Content/TO-DELETE-SecureCloud Resources/Custom/custom-topnav.js
@@ -0,0 +1,12 @@
+﻿/* Version: 1.0 */
+$(function() { //jQuery ready function
+	// Set theme
+	var theme = $('#var-theme').text();
+	$('body').addClass('theme-' + theme);
+	
+	// Remove header
+	//$('.homepage nav.title-bar').remove();
+		
+	// Add target _blank to external links
+	$('a[href^="http"]').attr('target', '_blank');
+});
