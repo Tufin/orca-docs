e3224d1778b62597cb2584e62b81334de8c64c70
Neil Leigh <neil.leigh@tufin.com>
2020-01-30 17:15:59 +0200

    feature / neil / birth of securecloud

--- Project/Advanced/CSH/CloudAlias.h
+++ Project/Advanced/CSH/CloudAlias.h
@@ -1,29 +1,53 @@
 
-#define Identifier 0
-#define SCL_Deploy 5000
-#define SCL_ServiceGraph 5001
-#define SCL_Pods 5002
-#define SCL_Services 5003
-#define SCL_Nodes 5004
-#define SCL_Policy 5005
-#define SCL_Settings 5006
-#define SCL_UnderstandingOrca 5007
-#define SCL_Vulnerabilities 5008
-#define SCL_WhatsNew 5009
-#define SCL_AllowedConnections 5010
-#define SCL_KubernetesNetworkPolicies 5011
-#define SCL_FirewallTickets 5012
-#define SCL_OverlyPermissiveRpt 5013
-#define SCL_SecurityScore 5014
-#define SCL_FilteringGeneral 5015
-#define SCL_DiscoveryEngine 5016
-#define SCL_AddingConnection 5017
-#define SCL_Notifications 5018
-#define SCL_Dashboard 5019
-#define SCL_AppManager 5020
-#define SCL_Applications 5021
-#define SCL_Assets 5022
-#define SCL_Global 5023
-#define SCL_TagManager 5024
-#define SCL_CloudPolicy 5025
-#define SCL_Notifications 5026
+#define Identifier (#cshid=SCL_SignUp) Ignore
+#define SCL_SignUp 5001
+#define SCL_WhatsNew 5002
+#define SCL_Dashboard 5003
+#define SCL_UserManagement 5004
+#define  5005
+#define SCL_K8sContainerVulnerabilities-by-Cluster 5006
+#define SCL_K8sContainerVulnerabilities-by-Namespace 5007
+#define SCL_K8sContainerVulnerabilities-by-Pod 5008
+#define SCL_K8sPermissivePolicies-by-Cluster 5009
+#define SCL_K8sPermissivePolicies-by-Namespace 5010
+#define SCL_K8sPermissive-Policies-in-Namespace 5011
+#define SCL_K8sPrivilegedWorkloads-by-Cluster 5012
+#define SCL_K8sPrivilegedWorkloads-by-Namespace 5013
+#define SCL_K8sPrivilegedWorkloads-by-Pod 5014
+#define SCL_K8sVulnerabilities-by-Cluster 5015
+#define SCL_K8sVulnerabilities-in-Cluster 5016
+#define  5017
+#define SCL_CloudPermissiveAccess-by-App 5018
+#define SCL_CloudPermissiveAccess-by-Asset 5019
+#define SCL_CloudPolicyViolations-by-App 5020
+#define SCL_CloudPolicyViolations-by-Asset 5021
+#define SCL_CloudRiskyPorts-by-App 5022
+#define SCL_CloudRiskyPorts-by-Asset 5023
+#define SCL_CloudTagUsage-by-Asset 5024
+#define  5025
+#define SCL_Deploy 5026
+#define SCL_ServiceGraph 5027
+#define SCL_Pods 5028
+#define SCL_Services 5029
+#define SCL_Nodes 5030
+#define SCL_Policy 5031
+#define SCL_Settings 5032
+#define SCL_UnderstandingOrca 5033
+#define SCL_Vulnerabilities 5034
+#define SCL_AllowedConnections 5035
+#define SCL_KubernetesNetworkPolicies 5036
+#define SCL_FirewallTickets 5037
+#define  
+#define SCL_SecurityScore 5039
+#define SCL_FilteringGeneral 5040
+#define SCL_DiscoveryEngine 5041
+#define SCL_AddingConnection 5042
+#define SCL_Notifications 5043
+#define  5044
+#define SCL_AppManager 5045
+#define SCL_Applications 5046
+#define SCL_Assets 5047
+#define SCL_Global 5048
+#define SCL_TagManager 5049
+#define SCL_CloudPolicy 5050
+#define SCL_Notifications 5051
