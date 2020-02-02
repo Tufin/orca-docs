07a3b8a11d53b0976273ac253015f8896c6d0261
Neil Leigh <neil.leigh@tufin.com>
2020-02-02 08:05:57 +0200

    feature / neil / birth of securecloud

--- Project/Advanced/CSH/CloudAlias.h
+++ Project/Advanced/CSH/CloudAlias.h
@@ -4,7 +4,7 @@
 #define SCL_WhatsNew 5002
 #define SCL_Dashboard 5003
 #define SCL_UserManagement 5004
-#define  5005
+#define  
 #define SCL_K8sContainerVulnerabilities-by-Cluster 5006
 #define SCL_K8sContainerVulnerabilities-by-Namespace 5007
 #define SCL_K8sContainerVulnerabilities-by-Pod 5008
@@ -16,7 +16,6 @@
 #define SCL_K8sPrivilegedWorkloads-by-Pod 5014
 #define SCL_K8sVulnerabilities-by-Cluster 5015
 #define SCL_K8sVulnerabilities-in-Cluster 5016
-#define  5017
 #define SCL_CloudPermissiveAccess-by-App 5018
 #define SCL_CloudPermissiveAccess-by-Asset 5019
 #define SCL_CloudPolicyViolations-by-App 5020
@@ -24,7 +23,6 @@
 #define SCL_CloudRiskyPorts-by-App 5022
 #define SCL_CloudRiskyPorts-by-Asset 5023
 #define SCL_CloudTagUsage-by-Asset 5024
-#define  5025
 #define SCL_Deploy 5026
 #define SCL_ServiceGraph 5027
 #define SCL_Pods 5028
@@ -37,17 +35,16 @@
 #define SCL_AllowedConnections 5035
 #define SCL_KubernetesNetworkPolicies 5036
 #define SCL_FirewallTickets 5037
-#define  
 #define SCL_SecurityScore 5039
 #define SCL_FilteringGeneral 5040
 #define SCL_DiscoveryEngine 5041
 #define SCL_AddingConnection 5042
 #define SCL_Notifications 5043
-#define  5044
-#define SCL_AppManager 5045
 #define SCL_Applications 5046
 #define SCL_Assets 5047
-#define SCL_Global 5048
+#define SCL_GlobalMap 5048
 #define SCL_TagManager 5049
 #define SCL_CloudPolicy 5050
-#define SCL_Notifications 5051
+#define SCL_CloudConfiguration 5051
+#define SCL_temp1 5052
+#define SCL_temp2 5053
