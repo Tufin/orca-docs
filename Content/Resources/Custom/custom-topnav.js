3ffb92086a52de46e285bf9cd9af6fdc149c5ddb
Neil Leigh <neil.leigh@tufin.com>
2020-06-29 15:40:07 +0300

    fix / jeff / global

--- Content/Resources/Custom/custom-topnav.js
+++ Content/Resources/Custom/custom-topnav.js
@@ -3,10 +3,29 @@ $(function() { //jQuery ready function
 	// Set theme
 	var theme = $('#var-theme').text();
 	$('body').addClass('theme-' + theme);
-	
-	// Remove header
-	//$('.homepage nav.title-bar').remove();
-		
+	//Add dropdown
+	var version = $('#var-version').text();
+	if (version=='true') addVersionDropdown();
 	// Add target _blank to external links
 	$('a[href^="http"]').attr('target', '_blank');
+	//add title
+	addMyTitle();
 });
+
+// Supporting functions
+function addVersionDropdown() {
+  var urlPrefix = (location.href.indexOf('localhost') !== -1) ? '/xampp' : '';
+  $.get(urlPrefix+'/support/kc/alexander.htm', null, function(data, status) {
+    $version = $(data).filter('.version-data');
+    $version[0].removeAttribute('style');
+    $('<div class="version-dropdown"></div>')
+      .append($version[0])
+	  .insertAfter('.search-bar');
+	  $('.search-bar').addClass('search-version');
+  });
+}
+
+
+function addMyTitle () {
+	$("#header-title").appendTo("div.logo-wrapper");
+}
